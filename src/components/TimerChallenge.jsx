0a2a19b28db12b4bdad8c71964378f36f62da2d1
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-13 13:05:00 +0700

    update TimerChallenge component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,31 @@
+import { useState, useRef } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const dialog = useRef();
+  const timer = useRef();
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    //show modal
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(timer.current);
+    //show modal
+  }
+
   return (
     <>
       <section className="challenge">
@@ -7,9 +34,13 @@ export default function TimerChallenge({ title, targetTime }) {
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button>Stop / Start</button>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"}
+          </button>
         </p>
-        <p className="">Time is running... / Time is inactive</p>
+        <p className={timerIsActive ? "active" : null}>
+          {timerIsActive ? "Time is running..." : "Time is inactive"}
+        </p>
       </section>
     </>
   );
