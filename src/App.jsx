6a821d4891378a2600979ad94db5aa01e1763673
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-15 10:35:13 +0700

    create modal component

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,10 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
