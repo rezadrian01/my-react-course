c58a634a5c4653543fc89ffd837f17662b88cb87
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-15 09:33:17 +0700

    create fallback content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
