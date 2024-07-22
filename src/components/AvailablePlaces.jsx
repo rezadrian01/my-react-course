5c383b97646a587d04fd2e4c389b42d5b6b81f75
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-22 14:15:45 +0700

    create error handling when fetching places

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,19 +1,38 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
   useEffect(() => {
     async function fetchPlaces() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        setIsFetching(true);
+        const response = await fetch("http://localhost:3000/placess");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error.message} />;
+  }
+
   return (
     <Places
       title="Available Places"
