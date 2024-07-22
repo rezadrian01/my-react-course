7e5085634edbabcbdcfb7c9faf56cd0c33dbd46c
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-22 14:01:00 +0700

    add loading text when fetching places

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -15,6 +18,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
