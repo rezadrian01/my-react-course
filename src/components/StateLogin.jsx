2a435c18ebaf304833bd49a07eca79db8474dd88
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-24 19:44:38 +0700

    using onBlur event

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -5,10 +5,26 @@ export default function Login() {
     email: "",
     password: "",
   });
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+  const emailIsInvalid = didEdit.email && !enteredValue.email.includes("@");
   function handleInputChange(identifier, value) {
     setEnteredValue((prevValue) => ({
       ...prevValue,
       [identifier]: value,
+    }));
+
+    setDidEdit((prevValue) => ({
+      ...prevValue,
+      [identifier]: false,
+    }));
+  }
+  function handleInputBlur(identifier) {
+    setDidEdit((prevValue) => ({
+      ...prevValue,
+      [identifier]: true,
     }));
   }
   function handleSubmit(event) {
@@ -27,10 +43,14 @@ export default function Login() {
           <input
             value={enteredValue.email}
             onChange={(event) => handleInputChange("email", event.target.value)}
+            onBlur={() => handleInputBlur("email")}
             id="email"
             type="email"
             name="email"
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
@@ -40,6 +60,7 @@ export default function Login() {
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
+            onBlur={() => handleInputBlur("password")}
             id="password"
             type="password"
             name="password"
