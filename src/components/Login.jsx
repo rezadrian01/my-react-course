ed627cffd01521be683f6a3a66c717c68c6f117d
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-24 19:55:03 +0700

    create confirm password validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log(
       `Email: ${email.current.value} \nPassword: ${password.current.value}`
     );
+    const emailIsValid = email.current.value.includes("@");
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+    setEmailIsInvalid(false);
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -18,6 +25,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input ref={email} id="email" type="email" name="email" />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordAreNotEqual, setPasswordAreNotEqual] = useState(false);
   function handleSubmit(event) {
     event.preventDefault();
+
     const fd = new FormData(event.target);
     const acquisitionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisitionChannel = acquisitionChannel;
+    if (data.password !== data["confirm-password"]) {
+      console.log("Password are not match");
+      setPasswordAreNotEqual(true);
+      return;
+    }
     console.log(data);
     // event.target.reset();
   }
@@ -15,13 +24,19 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            minLength={6}
+          />
         </div>
 
         <div className="control">
@@ -30,7 +45,12 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            required
+            minLength={6}
           />
+          <div className="control-error">
+            {passwordAreNotEqual && <p>Password must to be match.</p>}
+          </div>
         </div>
       </div>
 
@@ -39,18 +59,18 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" required />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" required>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
