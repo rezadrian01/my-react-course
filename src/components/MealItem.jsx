fb65acdc7fa7cebfff959976d6ca616fa09cba85
rezadrian01 <ahmadadrian324@gmail.com>
2024-07-30 08:27:29 +0700

    create button component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,5 @@
 import { currencyFormatter } from "../utils/formatting";
+import Button from "./UI/Button";
 
 export default function MealItem({ meal }) {
   return (
@@ -13,7 +14,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-desription">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add To Cart</button>
+          <Button>Add To Cart</Button>
         </p>
       </article>
     </li>
