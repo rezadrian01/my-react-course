4c3978b1d4ac531e29204ff8cd125fa6e6185842
rezadrian01 <ahmadadrian324@gmail.com>
2024-08-01 09:59:47 +0700

    create store and slice

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  function handleCartClick() {
+    dispatch(uiActions.toggle());
+  }
   return (
-    <button className={classes.button}>
+    <button onClick={handleCartClick} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
