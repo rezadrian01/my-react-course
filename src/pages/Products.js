3d3590620dc904d6207cd2bdfba475242879a710
rezadrian01 <ahmadadrian324@gmail.com>
2024-08-02 19:04:22 +0700

    output dymanic path

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,32 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "Product 1",
+  },
+  {
+    id: "p2",
+    title: "Product 2",
+  },
+  {
+    id: "p3",
+    title: "Product 3",
+  },
+];
 export default function ProductsPage() {
-  return <h1>The Products Page</h1>;
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <ul>
+        {PRODUCTS.map((prod) => {
+          return (
+            <li key={prod.id}>
+              <Link to={`/product/${prod.id}`}>{prod.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
