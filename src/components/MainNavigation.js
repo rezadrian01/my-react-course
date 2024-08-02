378c0461a6888e3b91e8441efdc29f53a4fab3e0
rezadrian01 <ahmadadrian324@gmail.com>
2024-08-02 18:39:06 +0700

    using NavLink in nav

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 export default function MainNavigation() {
@@ -7,10 +7,21 @@ export default function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to={"/"}>Home</Link>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => (isActive ? classes.active : null)}
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to={"/products"}>Products</Link>
+            <NavLink
+              to={"/products"}
+              className={({ isActive }) => (isActive ? classes.active : null)}
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
