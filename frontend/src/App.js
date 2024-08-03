198476a1647afec101c0190d13ec703127fff7df
rezadrian01 <ahmadadrian324@gmail.com>
2024-08-03 17:29:50 +0700

    added some component and use defer to load event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,29 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./pages/Root";
-import HomePage from "./pages/Home";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
 import EventDetailPage, {
-  loader as eventLoader,
+  loader as eventDetailLoader,
   action as deleteEventAction,
-} from "./pages/EventDetail.js";
-import NewEventPage from "./pages/NewEvent";
-import EditEventPage from "./pages/EditEvent";
-import EventsRootLayouts from "./pages/EventsRoot.js";
-import ErrorPage from "./pages/Error.js";
-import { action as manipulateEvent } from "./components/EventForm.js";
-
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-//DONE
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-//DONE
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-//DONE
-// 4. Add properly working links to the MainNavigation
-//DONE
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-//DONE
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-//DONE
-// 7. Output the ID of the selected event on the EventDetailPage
-//DONE
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-//DONE
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "events",
-        element: <EventsRootLayouts />,
+        path: 'events',
+        element: <EventsRootLayout />,
         children: [
           {
             index: true,
@@ -59,9 +31,9 @@ const router = createBrowserRouter([
             loader: eventsLoader,
           },
           {
-            path: ":eventId",
-            id: "event-detail",
-            loader: eventLoader,
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
             children: [
               {
                 index: true,
@@ -69,15 +41,23 @@ const router = createBrowserRouter([
                 action: deleteEventAction,
               },
               {
-                path: "edit",
+                path: 'edit',
                 element: <EditEventPage />,
-                action: manipulateEvent,
+                action: manipulateEventAction,
               },
             ],
           },
-
-          { path: "new", element: <NewEventPage />, action: manipulateEvent },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,26 +1,37 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
   return (
     <>
-      <EventsList events={events} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function fetchEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     //...
     throw json({ message: "Could not fetch events" }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: fetchEvents(),
+  });
 }
